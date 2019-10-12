484292a578c4a3a307d6c888e49f571d6ce86998
liaolishan <306478548@qq.com>
2019-10-12 21:36:12 +0800

    day02

--- gamll-user/src/main/java/com/bdqn/gamll/user/mapper/UmsMemberReceiveAddressMapper.java
+++
@@ -1,7 +0,0 @@
-package com.bdqn.gamll.user.mapper;
-
-import com.bdqn.gamll.bean.UmsMemberReceiveAddress;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface UmsMemberReceiveAddressMapper extends Mapper<UmsMemberReceiveAddress> {
-}
---
+++ gmall-user-service/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.atguigu.gmall.user.service.impl;
+
+
+import com.atguigu.gmall.bean.UmsMember;
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.service.UserService;
+
+import com.atguigu.gmall.user.mapper.UmsMemberReceiveAddressMapper;
+import com.atguigu.gmall.user.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.alibaba.dubbo.config.annotation.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    UmsMemberReceiveAddressMapper umsMemberReceiveAddressMapper;
+
+    @Override
+    public List<UmsMember> getAllUser() {
+        List<UmsMember> umsMemberList= userMapper.selectAll();//userMapper.selectAllUser();
+        return umsMemberList;
+    }
+
+
+    @Override
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId) {
+
+//        UmsMemberReceiveAddress umsMemberReceiveAddress=new UmsMemberReceiveAddress();
+//        umsMemberReceiveAddress.setMemberId(memberId);
+        Example e=new Example(UmsMemberReceiveAddress.class);
+        e.createCriteria().andEqualTo("memberId",memberId);
+        return umsMemberReceiveAddressMapper.selectByExample(e);
+    }
+}
